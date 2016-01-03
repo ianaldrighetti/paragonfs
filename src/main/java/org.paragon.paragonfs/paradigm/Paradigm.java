ed53385a5fd6c07fc235e000db41e53584316c08
Ian Aldrighetti <ianaldrighetti@outlook.com>
2016-01-02 19:01:36 -0800

    setValues added.

--- src/main/java/org.paragon.paragonfs/paradigm/Paradigm.java
+++ src/main/java/org.paragon.paragonfs/paradigm/Paradigm.java
@@ -3,12 +3,18 @@ package org.paragon.paragonfs.paradigm;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.paragon.paragonfs.archetype.Archetype;
+import org.paragon.paragonfs.data.type.DataType;
 import org.paragon.paragonfs.data.type.DataTypeIF;
 import org.paragon.paragonfs.exception.ParagonFSException;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -23,6 +29,8 @@ public class Paradigm
 {
 	public static final String PARADIGM_KEY_TYPE = "type";
 	public static final String PARADIGM_KEY_VALUE = "value";
+	public static final String DATA = "data";
+
 	/**
 	 * The {@link Archetype} this Paradigm belongs to.
 	 */
@@ -51,8 +59,8 @@ public class Paradigm
 	/**
 	 * Initializes the {@link Paradigm}.
 	 *
-	 * @param archetype The archetype this Paradigm belongs to.
-	 * @param file The file which this Paradigm represents.
+	 * @param archetype  The archetype this Paradigm belongs to.
+	 * @param file       The file which this Paradigm represents.
 	 * @param paradigmId The ID of the Paradigm.
 	 */
 	public Paradigm(final Archetype archetype, final File file, final String paradigmId)
@@ -96,10 +104,10 @@ public class Paradigm
 	/**
 	 * Sets the value of a single key on the Paradigm.
 	 *
-	 * @param key The key to set the value on.
+	 * @param key   The key to set the value on.
 	 * @param value The value to set the key to.
 	 */
-	public void setValue(final String key, final DataTypeIF value)
+	public void setValue(final String key, final DataTypeIF value) throws ParagonFSException
 	{
 		final Map<String, DataTypeIF> values = new LinkedHashMap<>();
 		values.put(key, value);
@@ -112,22 +120,157 @@ public class Paradigm
 	 *
 	 * @param values A map of keys and their values to set.
 	 */
-	public void setValues(final Map<String, DataTypeIF> values)
+	public void setValues(final Map<String, DataTypeIF> values) throws ParagonFSException
 	{
 		// TODO validate keys are not empty, then write lock, update values, flush/sync.
+		validate(values);
+
+		// Obtain a write lock.
+		lock.writeLock().lock();
+
+		try
+		{
+			refresh();
+
+			if (object == null)
+			{
+				object = getEmptyObject();
+			}
+
+			@SuppressWarnings("unchecked")
+			final Map<String, Object> data = (Map<String, Object>) object.get(DATA);
+			for (final Map.Entry<String, DataTypeIF> entry : values.entrySet())
+			{
+				object.put(entry.getKey(), entry.getValue());
+			}
+
+			updateMetaData();
+			flush();
+		}
+		catch (final IOException e)
+		{
+			throw new ParagonFSException("An error occurred while refreshing the object.", e);
+		}
+		finally
+		{
+			lock.writeLock().unlock();
+		}
+	}
+
+	/**
+	 * Ensures that the map is not null or empty, along with ensuring there are no empty keys.
+	 *
+	 * @param values The map to validate.
+	 */
+	private void validate(final Map<String, DataTypeIF> values)
+	{
+		if (values == null)
+		{
+			throw new IllegalArgumentException("The values map must not be null.");
+		}
+		else if (values.isEmpty())
+		{
+			throw new IllegalArgumentException("The values map must not be empty.");
+		}
+
+		int index = 0;
+		for (final Map.Entry<String, DataTypeIF> entry : values.entrySet())
+		{
+			if (StringUtils.isNotBlank(entry.getKey()))
+			{
+				index++;
+				continue;
+			}
+
+			throw new IllegalArgumentException("The " + (index + 1) + " entry of the map has an empty key.");
+		}
+	}
+
+	/**
+	 * Returns an empty object, to be used when the file has not been created yet.
+	 *
+	 * @return An empty object with the version, timestamp and data maps.
+	 */
+	private Map<String, Object> getEmptyObject()
+	{
+		final Map<String, Object> object = new LinkedHashMap<>();
+
+		// A version of 0, it will be incremented anyways.
+		object.put("version", 0);
+
+		// Add the map with the created timestamp only -- updated will be overwritten as well.
+		final Map<String, Object> timestamp = new LinkedHashMap<>();
+		timestamp.put("created", Calendar.getInstance());
+		object.put("timestamp", timestamp);
+
+		// Then an empty map for data.
+		object.put("data", new LinkedHashMap<>());
+
+		return object;
+	}
+
+	/**
+	 * When invoked this method will increment the version number of the object and update the updated timestamp to now.
+	 */
+	private void updateMetaData()
+	{
+		final Integer version = (Integer) object.get("version");
+
+		// Increment the version as appropriate.
+		if (version == null) {
+			object.put("version", 1);
+		}
+		else {
+			object.put("version", version + 1);
+		}
+
+		@SuppressWarnings("unchecked")
+		final Map<String, Object> timestamp = (Map<String, Object>) object.get("timestamp");
+		timestamp.put("updated", Calendar.getInstance());
+	}
+
+	/**
+	 * This will flush the current state of the object onto disk. This method uses {@link FileOutputStream} and then
+	 * invokes {@link java.nio.channels.FileChannel#force(boolean)} and {@link FileDescriptor#sync()} to ensure the
+	 * file has been written to disk.
+	 *
+	 * @throws IOException
+	 */
+	private void flush() throws IOException
+	{
+		FileOutputStream stream = null;
+		try
+		{
+			// Open the file stream and write the object.
+			stream = new FileOutputStream(file, false);
+			stream.write(getObjectMapper().writeValueAsBytes(object));
+
+			stream.getChannel().force(true);
+			stream.getFD().sync();
+		}
+		finally {
+			if (stream == null) {
+				//noinspection ReturnInsideFinallyBlock
+				return;
+			}
+
+			stream.close();
+		}
 	}
 
 	/**
 	 * Returns a set of all the keys in the Paradigm.
 	 *
 	 * @return Returns a set of all keys in the Paradigm.
+	 *
 	 * @throws ParagonFSException
 	 */
 	public Set<String> keySet() throws ParagonFSException
 	{
 		lock.readLock().lock();
 
-		try {
+		try
+		{
 			refresh();
 
 			return new LinkedHashSet<>(object.keySet());
@@ -136,7 +279,8 @@ public class Paradigm
 		{
 			throw new ParagonFSException("An error occurred while refreshing the object.", e);
 		}
-		finally {
+		finally
+		{
 			lock.readLock().unlock();
 		}
 	}
@@ -146,26 +290,36 @@ public class Paradigm
 	 *
 	 * @param key The key to retrieve, case-insensitive.
 	 * @return The value of the key, or null if the key does not exist in this Paradigm. <strong>Note:</strong> The
-	 * 			value returned may not be null but the {@link DataTypeIF#getValue()} may also return null. This means
-	 * 			that the key was defined, but no value was set.
+	 * value returned may not be null but the {@link DataTypeIF#getValue()} may also return null. This means
+	 * that the key was defined, but no value was set.
+	 *
 	 * @throws ParagonFSException Thrown if an error occurs while trying to read from the file system.
 	 */
 	public DataTypeIF getValue(final String key) throws ParagonFSException
 	{
-		if (key == null) {
+		if (key == null)
+		{
 			throw new IllegalArgumentException("The key must not be null.");
 		}
 
 		lock.readLock().lock();
 
-		try {
+		try
+		{
 			// Ensure the object is loaded.
 			refresh();
 
+			// Make sure the object isn't empty.
+			if (object == null || !object.containsKey(DATA))
+			{
+				return null;
+			}
+
 			// Now get the value for the key, if any.
 			@SuppressWarnings("unchecked")
-			final Map<String, Object> value = (Map<String, Object>) object.get(key.toLowerCase());
-			if (value == null || !value.containsKey(PARADIGM_KEY_TYPE)) {
+			final Map<String, Object> value = (Map<String, Object>) ((Map<String, Object>) object.get(DATA)).get(key.toLowerCase());
+			if (value == null || !value.containsKey(PARADIGM_KEY_TYPE))
+			{
 				return null;
 			}
 
@@ -191,15 +345,40 @@ public class Paradigm
 	 */
 	private void refresh() throws IOException
 	{
-		if (object != null) {
+		if (object != null)
+		{
+			return;
+		}
+		else if (!file.exists())
+		{
 			return;
 		}
 
-		final JsonFactory jsonFactory = new JsonFactory();
-		final ObjectMapper mapper = new ObjectMapper(jsonFactory);
-		final TypeReference<Map<String, Object>> typeReference = new TypeReference<Map<String, Object>>() {};
+		object = getObjectMapper().readValue(file, getMapTypeReference());
+	}
 
-		object = mapper.readValue(file, typeReference);
+	/**
+	 * Returns an instance of an {@link ObjectMapper}.
+	 *
+	 * @return An object mapper.
+	 */
+	private ObjectMapper getObjectMapper()
+	{
+		final JsonFactory jsonFactory = new JsonFactory();
+
+		return new ObjectMapper(jsonFactory);
+	}
+
+	/**
+	 * Returns a {@link TypeReference} to be used with an {@link ObjectMapper}.
+	 *
+	 * @return {@link TypeReference}
+	 */
+	private TypeReference<Map<String, Object>> getMapTypeReference()
+	{
+		return new TypeReference<Map<String, Object>>()
+		{
+		};
 	}
 
 	/**
---
+++ src/test/java/org/paragon/paragonfs/paradigm/ParadigmTest.java
@@ -0,0 +1,8 @@
+package org.paragon.paragonfs.paradigm;
+
+/**
+ * Tests for {@link Paradigm}.
+ */
+public class ParadigmTest
+{
+}
